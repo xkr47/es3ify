849bea4aeb6542f3bc1e14db5dccbbf79a1a1a6d
Jonas Berlin <xkr47@outerspace.dyndns.org>
2016-09-05 22:25:37 +0300

    Add variable tests that should pass but fail

--- spec/es3ifyspec.js
+++ spec/es3ifyspec.js
@@ -31,8 +31,28 @@ describe('es3ify', function() {
                 .toEqual('({x: 3, y: 4})');
     });
 
+    it('should rename variables', function() {
+        expect(transform('var char = 3; char++;'))
+                .toEqual('var char__es3 = 3; char__es3++;');
+    });
+
+    it('should rename function argument variables', function() {
+        expect(transform('function c(char){return char+2}'))
+                .toEqual('function c(char__es3){return char__es3+2}');
+    });
+
+    it('should rename duplicate variable declarations correctly', function() {
+        expect(transform('var char = 3; var _char = 5; var char = 6;'))
+                .toEqual('var char__es3 = 3; var _char = 5; var char__es3 = 6;');
+    });
+
+    it('should rename function argument variable duplicate declarations correctly', function() {
+        expect(transform('function c(char){var char=5;return char+2}'))
+                .toEqual('function c(char__es3){var char__es3=5;return char__es3+2}');
+    });
+
     it('should transform everything at once', function() {
-        expect(transform('({a:2,\tfor :[2,,3,],}\n.class)'))
-                .toEqual('({a:2,\t"for" :[2,,3]}[\n"class"])');
+        expect(transform('var char=0;char({a:2,\tfor :[2,,3,],}\n.class)'))
+                .toEqual('var char__es3=0;char__es3({a:2,\t"for" :[2,,3]}[\n"class"])');
     });
 });
